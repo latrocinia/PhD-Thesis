89e14692bf606aeb75f69c41117c8755960462aa
latrocinia <g.c.p.vanzundert@uu.nl>
2015-05-11 20:36:24 +0200

    Update

--- scripts/txt2TABLE.py
+++ scripts/txt2TABLE.py
@@ -9,11 +9,9 @@ with open(sys.argv[1]) as fh, open(sys.argv[2], 'w') as out:
         if not line:
             continue
 
-        out.write('\\bTR\n')
-
-        words = line.split()
-        for word in words:
-            out.write('\\bTD ' + word + ' ''eTD ')
+        # split at | and remove white space
+        words = line.split('|')
+        data = ' \\eTD \\bTD '.join(words)
+        out.write('\\bTR\n\\bTD ' + data[:-1] + ' \\eTD\n\\eTR\n')
         
-        out.write('\n\\eTR\n')
     out.write('\\eTABLE\n')
